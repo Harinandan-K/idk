765787843ccff411f56bb02b54b6cc8316b1cb12
Harinandan-K <harinandankedambi@gmail.com>
2026-06-11 14:47:21 +0530

    ADD: Test folder, EDIT: started cli argparser learning

--- cli/parser.py
+++ cli/parser.py
@@ -1,5 +1,10 @@
-def square():
-        print('hiii!')
+def read():
+    command = str(input())
+    command_split = command.split()
+
+    if command_split[0] == "idk":
+        print("HI welcome to IDK")
+
 
 if __name__ == "__main__":
     pass
