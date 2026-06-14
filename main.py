6ff663c7c469020b7a6ed050d9375dfd5692a154
Harinandan-K <harinandankedambi@gmail.com>
2026-06-14 15:25:07 +0530

    EDIT: new base_check func. P.S: add LOGGING

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
-from cli import *
+#from cli import parser
+from core import bootstrap
 
 def main() -> None:
-   parser.read()
+   base_check: str = bootstrap.base_checks()
+   if  base_check.startswith('ERR_'):
+      #call the cli to show the errors
+      print(base_check)
+   
+   print(base_check)
 
 if __name__ == "__main__":
    main() 
