2daaba8a08c8eac47be106988b26529c00752baa
Harinandan-K <harinandankedambi@gmail.com>
2026-06-14 15:25:07 +0530

    EDIT: new base_check func. P.S: add LOGGING

--- core/bootstrap.py
+++ core/bootstrap.py
@@ -1,14 +1,36 @@
-import sys, os
-
-def os_check() -> None:
-    if  sys.platform == "linux":
-        print("ADD TO LOG - os type")
+import sys
+import os
+   
+def os_check() -> str:
+    compatable_os: list[str] = ["linux", "darwin"]
+    if  sys.platform in compatable_os:
+        return('OK_OS_SUPPORTED') #LOG 
     else:
-        pass
+        return('ERR_OS_NOT_SUPPORTED') #LOG
 
-def shell_check() -> None:
-    print(os.environ)
+def shell_check() -> str:
+    compatable_shell: list[str] = ['bash', 'zsh', 'fish']
+    shell_dir: str | None = os.environ.get("SHELL")
+    if  shell_dir != None:
+        shell_dir_split: list[str] = shell_dir.split('/')
+    else:
+        return('ERR_NO_SHELL_FOUND') #LOG
+    shell_list: str = shell_dir_split[-1]
+    if  shell_list in compatable_shell:
+        return('OK_SHELL_SUPPORTED') #LOG
+    else:
+        return('ERR_SHELL_NOT_SUPPORTED') #LOG
 
-if __name__ == "__main__":
-    print(sys.platform)
-    print(os.environ.get("SHELL"))
+def base_checks() -> str :
+    return_os_check: str = os_check()
+    if  return_os_check.startswith("ERR_"):
+        return return_os_check
+    return_shell_check: str = shell_check()
+    if  return_shell_check.startswith("ERR_"):
+        return return_shell_check
+    
+    return ('OK_ALL_CHECK_SUCCESS')
+
+
+if  __name__ == "__main__":
+    base_checks()
