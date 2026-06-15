6c93f98a4084112c8857e22823cfc3cb27db8c09
Harinandan-K <harinandankedambi@gmail.com>
2026-06-15 22:08:53 +0530

    EDIT: new log format(log_config.py), updated checks (bootstrap.py)

--- core/log_config.py
+++ core/log_config.py
@@ -2,13 +2,16 @@ from typing import Any
 
 LOGGING_CONFIG: dict[str, Any] = {
     'version': 1,
+    'disable_existing_loggers': False,
     'formatters': {
         'custom_style': {
-            'format': '[%(levelname)s] [%(filename)s] "%(message)s" [Line No: %(lineno)d by %(name)s]'
+            'format': '[%(asctime)s] [%(levelname)s] "%(message)s" [%(filename)s:%(funcName)s:%(lineno)d]',
+            'datefmt': '%d/%m/%y %H:%M:%S'
         }
     },
     'handlers': {
         'file_worker': {
+            'mode': 'w',
             'class': 'logging.FileHandler',
             'filename': 'logs/idk_debug.log',
             'formatter': 'custom_style',
