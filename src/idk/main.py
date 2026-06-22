580703488840af039eb984d86dc9e5be0007c540
Harinandan-K <harinandankedambi@gmail.com>
2026-06-22 22:11:45 +0530

    feat(core)(main): started working on the cli flags

--- src/idk/main.py
+++ src/idk/main.py
@@ -1,36 +1,15 @@
 import logging.config
-<<<<<<< HEAD
-from idk.core import bootstrap,log_config, setup_wizard
-=======
 from idk.core import bootstrap, history_collector, log_config, setup_wizard, history_cleaner
->>>>>>> 803604ee6976fdf3adf084860a4e5f9118d50edc
+from idk.cli import cli
 
 
 def main() -> None:
 
+   cli.run()
+
    #setting up the logger
    logging.config.dictConfig(log_config.LOGGING_CONFIG)
    logger = logging.getLogger(__name__)
-<<<<<<< HEAD
-   logger.info("Logging successfully started")
-
-   #perform the base_check 
-   base_check_return: str = bootstrap.base_checks()
-   if  base_check_return.startswith('FATAL_'):
-      logger.debug("Need to pass the ERR cli module")
-   else:
-      logger.info(base_check_return)
-
-   #setup wizard (runs if new install)
-   setup_caller_return: str = setup_wizard.setup_caller()
-   if  setup_caller_return.startswith('FATAL_'):
-      logger.debug("Need to pass the ERR cli module")
-   else:
-      logger.info(setup_caller_return)
-
-
-if __name__ == "__main__":
-=======
    logger.info('INFO_LOGGING_STARTED')
    logger.info('INFO_LOGGING_MAIN')
 
@@ -42,7 +21,6 @@ if __name__ == "__main__":
    else:
       logger.info(base_check_return)
 
-
    #setup wizard round 1
    setup_caller_return: str = setup_wizard.setup_caller()
    if  setup_caller_return.startswith('FATAL_'):
@@ -50,7 +28,7 @@ if __name__ == "__main__":
    else:
       logger.info(setup_caller_return)
 
-   #read shell histrory from ./<shell>_history
+   #read shell history from ./<shell>_history
    history_collector_return: str = history_collector.history_collector()
    if  history_collector_return.startswith('FATAL_'):
       logger.debug('Need to pass the ERR cli module')
@@ -65,6 +43,4 @@ if __name__ == "__main__":
       logger.info(history_cleaner_return)
 
 if __name__ == '__main__':
->>>>>>> 803604ee6976fdf3adf084860a4e5f9118d50edc
    main()
-   
